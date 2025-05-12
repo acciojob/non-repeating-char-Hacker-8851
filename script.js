d40d14c7f93b41c26559c4ae1309540720040213
Hacker-8851 <kumarsachin8851311@gmail.com>
2025-05-12 10:42:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 function firstNonRepeatedChar(str) {
- const charCount = {};
+    const charCount = {};
 
-  
-  for (let char of str) {
-    charCount[char] = (charCount[char] || 0) + 1;
-  }
-
-  for (let char of str) {
-    if (charCount[char] === 1) {
-      return char;
+    
+    for (let char of str) {
+        charCount[char] = (charCount[char] || 0) + 1;
     }
-  }
 
-  return null;
+    for (let char of str) {
+        if (charCount[char] === 1) {
+            return char;
+        }
+    }
+    return null;
 }
-}
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+
+const input = prompt("Enter a string:");
+alert(firstNonRepeatedChar(input));
